44462023451e4267698c58db398033d76d9f2b0c
Denis Kustov <kustov.dv@gmail.com>
2019-08-13 11:04:24 +0700

    readme finished, small changes in 0213, 0214 added

--- mB2-python-02/script-b0213-03.py
+++ mB2-python-02/script-b0213-03.py
@@ -10,3 +10,50 @@
 # либо их выгрузка займет слишком много времени. Другая часть данных может оказаться чувствительной
 # и её нельзя отправлять в сторонние сервисы.
 
+import collections
+
+FILENAME = "data/dummy-access.log"
+
+fp = open(FILENAME, encoding='UTF-8')
+text_lines = fp.readlines()
+fp.close()
+
+# Вопрос 1, Вопрос 2 ----------------
+# подсчитать количество запросов от ip
+counter = collections.defaultdict(int)
+ip =['79.136.245.135', '127.0.0.1']
+
+for line in text_lines:
+    if ip[0] in line:
+        counter[ip[0]] += 1
+    if ip[1] in line:
+        counter[ip[1]] += 1
+print(counter)
+
+print("-------------------------------------------------------------")
+
+# Вопрос 3 ----------------
+# Каково максимальное количество запросов от одного клиента? Введите число запросов:
+counter_ip = collections.Counter()
+
+for line in text_lines:
+    words = line.split()
+    #    print(words)
+    #    print(words[0])
+    counter_ip[words[0]] += 1
+
+# используем метод most_common(10)
+# для вывода 10 наиболее частых слов
+for ip, cnt in counter_ip.most_common(10):
+    print("{} -- {}".format(ip, cnt))
+
+average_requests_qty = sum(counter_ip.values()) / len(counter_ip.values())
+print("Среднее количество запросов от ip: ", round(average_requests_qty,2))
+
+#Максимальное значение и его ключ
+print("Max запросов: ", max(counter_ip.values()))
+print("Ключ для max запросов", max(counter_ip, key=counter_ip.get)) # Ключ Get dict key by max value
+
+#Максимальное значение и его ключ
+print("Min запросов: ", min(counter_ip.values()))
+print("Ключ для min запросов (первый)", min(counter_ip, key=counter_ip.get)) # Ключ Get dict key by max value
